f0c2748cb813e08902c12b371e2ca2f4b878c2f2
Niko Tepe <tepe@domainprofi.com>
2025-04-09 08:51:34 +0200

    changed keybind

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,10 +3,10 @@
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -18,10 +18,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -29,20 +29,20 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- vim: ts=2 sts=2 sw=2 et
 
 -- <Leader> e -> Explore
-vim.keymap.set('n', '<leader>e', ':Explore<CR>', { desc = 'Open Explorer' })
+vim.keymap.set("n", "<leader>e", ":Explore<CR>", { desc = "Open Explorer" })
 -- <Leader> j -> hsplit + terminal
-vim.keymap.set('n', '<leader>j', ':split<CR>:terminal<CR>i', { desc = 'Open Terminal in Horizontal Split' })
+vim.keymap.set("n", "<leader>j", ":split<CR>:terminal<CR>i", { desc = "Open Terminal in Horizontal Split" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -50,9 +50,9 @@ vim.keymap.set('n', '<leader>j', ':split<CR>:terminal<CR>i', { desc = 'Open Term
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-vim.keymap.set('t', ':', '<C-\\><C-n>:', { desc = 'Exit terminal and go into Ex-Mode' })
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set("t", "<C-q>", "<C-\\><C-n>:", { desc = "Exit terminal and go into Ex-Mode" })
 
 -- Traverse quickfixes
-vim.keymap.set('n', '<C-j>', ':cnext<CR>', { desc = 'Move to next item in quickfix list' })
-vim.keymap.set('n', '<C-k>', ':cprev<CR>', { desc = 'Move to prev item in quickfix list' })
+vim.keymap.set("n", "<C-j>", ":cnext<CR>", { desc = "Move to next item in quickfix list" })
+vim.keymap.set("n", "<C-k>", ":cprev<CR>", { desc = "Move to prev item in quickfix list" })
